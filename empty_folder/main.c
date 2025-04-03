1de4ab60d7e6fc0d098445f6ed2e9ca6b2949e59
02guolicheng <1185044776@qq.com>
2025-04-03 10:59:07 +0800

    cxb 10:58:06

---
+++ empty_folder/main.c
@@ -0,0 +1,11 @@
+//¥Ú”°hello world
+#include <stdio.h>
+int main()
+{
+    printf("hello world\n");
+    //¥Ú”° I love you
+    printf("I love you\n");
+    printf("10:58:06\n");
+    return 0;
+
+}
